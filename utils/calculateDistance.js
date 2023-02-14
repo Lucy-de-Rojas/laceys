7563e6cc615c0ae3d26e2eeac13ae22e858f0604
Lucy de Rojas <lucyderojas@gmail.com>
2023-02-14 18:53:36 +0000

    'auto-update'

--- utils/calculateDistance.js
+++ utils/calculateDistance.js
@@ -1,28 +1,36 @@
+
+// Bolter End Farm, Finings Rd, Lane End, High Wycombe HP14 3LP
+
 // calculate distance between 2 points based on long and lat coords:
-export function getDistance(lat1, lat2, lon1, lon2) {
+export function getDistanceFromLaceys(lat, long) {
+        
 
-        lat1 = parseFloat(lat1);
-        lat1 = parseFloat(lat2);
+        const laceysLat =  51.621306;
+        const laceysLong = -0.845643;
 
-        lon1 = parseFloat(lon1);
-        lon2 = parseFloat(lon2);
+
+        lat = parseFloat(lat);
+        long = parseFloat(long);
 
 
         const R = 6371e3; // metres
 
-        const φ1 = lat1 * Math.PI/180; // φ, λ in radians
-        const φ2 = lat2 * Math.PI/180;
+        const φ1 = laceyLat * Math.PI/180; // φ, λ in radians
+        const φ2 = lat * Math.PI/180;
 
-        const Δφ = (lat2-lat1) * Math.PI/180;
-        const Δλ = (lon2-lon1) * Math.PI/180;
+        const Δφ = (lat-laceyLat) * Math.PI/180;
+        const Δλ = (long-laceyLong) * Math.PI/180;
 
+
+
+        
         const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) + Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ/2) * Math.sin(Δλ/2);
 
         const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
 
         const d = R * c; // in metres
 
-        console.log('distance is: >>>',d);
+        console.log('distance to Laceys is: >>>',d);
         return d;
 
 
