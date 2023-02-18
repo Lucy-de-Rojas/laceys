565132167f6b965bc27ce597b19c23a1a9a59044
Lucy de Rojas <lucyderojas@gmail.com>
2023-02-18 17:42:45 +0000

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+// /** @type {import('next-sitemap').IConfig} */
+
+
+module.exports = {
+    siteUrl: 'https://laceys.vercel.app/',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
